5fb178f6709352c12f1a7236df7dc45361d4c1cd
manaswinisreekar <you@example.com>
2020-10-19 15:32:37 +0530

    modified header file

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -5,41 +5,18 @@ import React, {Component, Fragment} from 'react';
 
 import './Header.css';
 
+import './Header.css';
+
 import {Input, InputAdornment} from "@material-ui/core";
+import {Avatar, IconButton, Input, InputAdornment} from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
-
-class Header extends Component {
-
-    render() {
-        return <div className='header-flex-container'>
-            <div>
-                <header className='logo'>Image Viewer</header>
-            </div>
+render() {
+    return <div className='header-flex-container'>
             {
                 this.props.isLoggedIn !== true ?
                     <div>
-                        <header className='logo'>Image Viewer</header>
+                            <header className='logo'>Image Viewer</header>
                     </div>
                     :
-                    <Fragment>
-                        <div>
-                            <header className='logo'>Image Viewer</header>
-                        </div>
-                        <div className='header-right-flex-container'>
-                            <Input className='search-box' type='search' placeholder='Search...' disableUnderline
-                                   startAdornment={<InputAdornment
-                                       position="start"><SearchIcon/></InputAdornment>}></Input>
-                        </div>
-                    </Fragment>
-
-
-            }
-        </div>
-
-
-    }
-}
-
-export default Header;
-
+     
