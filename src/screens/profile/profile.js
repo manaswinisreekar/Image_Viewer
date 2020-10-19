44f8dd05afb21ab1e27ac37969605119d354d49d
manaswinisreekar <you@example.com>
2020-10-19 18:56:35 +0530

    modal changes were implemented

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,38 +1,55 @@
+
 import React, {Component} from 'react'
 import Header from '../../common/header/Header';
 import {Redirect} from 'react-router-dom';
 import './Profile.css'
+import {red} from "@material-ui/core/colors";
 
-import {Avatar, Card, CardMedia,CardContent, Container, Fab,GridList,GridListTile, Typography, Grid} from '@material-ui/core/';
 import {
     Avatar,
     Button,
     Card,
     CardMedia,
+    CardActions,
     Container,
+    Divider,
     Fab,
     FormControl,
     FormHelperText,
+    IconButton,
     Input,
     InputLabel,
     Modal,
+    TextField,
     Typography,
     Grid
 } from '@material-ui/core/';
 
 import EditIcon from '@material-ui/icons/Edit'
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
- class Profile extends Component {
+class Profile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.baseUrl = 'https://api.instagram.com/v1/';
         this.state = {
             user_data: null,
             profile_picture: null,
-            recent_media: null
             recent_media: null,
             fullName: null,
             newFullName: '',
             fullNameRequired: false,
             openFullNameEditModal: false,
             closeFullNameEditModal: true
+            closeFullNameEditModal: true,
+            openImageDetailModal: false,
+            closeImageDetailModal: true,
+            imageSelectedForDetails: null,
+            indexImageSelectedForDetails: null,
+            liked: [],
+            comments: []
         }
     }
 
@@ -44,7 +61,6 @@ import EditIcon from '@material-ui/icons/Edit'
         if (this.props.location.state === undefined) {
             return <Redirect to='/'/>
         } else if (this.props.location.state.loginSuccess === true) {
-            console.log(this.state.recent_media)
             return <div>
                 <Header {...this.props} isLoggedIn={true} showSearchBox={false}
                         profilePictureUrl={this.state.profile_picture} showMyAccount={false}/>
@@ -63,7 +79,6 @@ import EditIcon from '@material-ui/icons/Edit'
                                 />
                             ) : null}
                         </Grid>
-                        <Grid item xs={4} id='user_name'>
                         <Grid item xs={5} id='user_name'>
                             <Typography variant="h4" component="h1" style={{marginBottom: 5}}>
                                 {this.state.recent_media
@@ -91,8 +106,6 @@ import EditIcon from '@material-ui/icons/Edit'
                                 </Grid>
                             </Grid>
                             <Typography variant="h6" component="h2" style={{marginTop: 5}}>
-                                {this.state.user_data ? this.state.user_data.full_name : null}
-                                {this.state.user_data && !this.state.user_data.full_name
                                 {this.state.fullName ? this.state.fullName : null}
                                 {this.state.user_data && !this.state.fullName
                                     ? this.state.user_data.full_name
@@ -106,7 +119,6 @@ import EditIcon from '@material-ui/icons/Edit'
                                     <EditIcon fontSize="small"/>
                                 </Fab>
                             </Typography>
-
                             <Modal
                                 open={this.state.openFullNameEditModal}
                                 onClose={this.closeEditFullNameModal}
@@ -126,7 +138,6 @@ import EditIcon from '@material-ui/icons/Edit'
                                     </div>
                                 </div>
                             </Modal>
-
                         </Grid>
                         <Grid item xs={4}/>
                     </Grid>
@@ -135,28 +146,114 @@ import EditIcon from '@material-ui/icons/Edit'
                     <Grid container spacing={1} direction="row" alignItems="center">
                         {this.state.recent_media &&
                         this.state.recent_media.map((mediaObj, index) => (
+                        this.state.recent_media.map((details, index) => (
                             <Grid
                                 // id="media-grids"
                                 item
                                 xs={4}
                                 key={mediaObj.id}>
+                                key={details.id}
+                                onClick={() => this.onImageClickForDetails(details, index)}>
                                 <Card variant="outlined">
-                                    <CardMedia style={{height: 0,paddingTop: '56.25%'}}
-                                        image={mediaObj.images.standard_resolution.url}
-                                        title={mediaObj.images.standard_resolution.url}/>
                                     <CardMedia style={{height: 0, paddingTop: '56.25%'}}
                                                image={mediaObj.images.standard_resolution.url}
                                                title={mediaObj.images.standard_resolution.url}/>
+                                               image={details.images.standard_resolution.url}
+                                               title={details.images.standard_resolution.url}/>
                                 </Card>
                             </Grid>
                         ))}
                     </Grid>
+
+                    <Modal
+                        open={this.state.openImageDetailModal}
+                        onClose={this.closeImageDetailsModal}
+                    >
+                        <div className="selected-image-modal">
+                            <Grid container spacing={1} direction="row" justify="center" alignItems="center">
+                                <Grid item xs={6}>
+                                    {this.state.imageSelectedForDetails ? (
+                                        <img alt={this.state.imageSelectedForDetails.images.id} src={this.state.imageSelectedForDetails.images.standard_resolution.url}
+                                            style={{height: "100%", width: "100%"}}/>
+                                    ) : null}
+                                </Grid>
+                                <Grid item xs={6} style={{minHeight: 'auto'}}>
+                                    {this.state.imageSelectedForDetails ? (
+                                        <div>
+                                            <Grid className="user-detail-section" container spacing={1} direction="row">
+                                                <Grid item xs={4}>
+                                                    <Avatar alt={this.state.imageSelectedForDetails.user.full_name}
+                                                            src={this.state.imageSelectedForDetails.user.profile_picture}
+                                                    />
+                                                </Grid>{" "}
+                                                <Grid item xs={8}>
+                                                    <Typography>
+                                                        {this.state.imageSelectedForDetails.user.username}
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                            <Divider/>
+                                            <Typography>
+                                                {this.state.imageSelectedForDetails.caption.text.split("\n")[0]}
+                                            </Typography>
+                                            <Typography>
+                                                {this.state.imageSelectedForDetails.tags.map(tag => {
+                                                    return (
+                                                        <span
+                                                            style={{color: "blue", fontSize: "14px"}}
+                                                            key={tag}
+                                                        >
+                                                    #{tag}{" "}
+                                                </span>
+                                                    );
+                                                })}
+                                            </Typography>
+                                            <Typography component="div" className="comment-section">
+                                                {this.state.comments &&
+                                                this.state.comments[this.state.indexImageSelectedForDetails] &&
+                                                this.state.comments[this.state.indexImageSelectedForDetails].length > 0 &&
+                                                this.state.comments[this.state.indexImageSelectedForDetails].map(comment => {
+                                                    return (
+                                                        <p style={{fontSize: 16, fontWeight: "bold"}} key={comment}>
+                                                            <b>{this.state.imageSelectedForDetails.user.username}:</b> {comment}
+                                                        </p>
+                                                    );
+                                                })}
+                                            </Typography>
+                                            <CardActions disableSpacing>
+                                                <IconButton aria-label="add to favorites"
+                                                            onClick={() => this.onLikeSelectedImage()}>
+                                                    {this.state.liked[this.state.indexImageSelectedForDetails] ?
+                                                        <FavoriteIcon style={{color: red[500]}}/>
+                                                        :
+                                                        <FavoriteBorderIcon/>}
+                                                </IconButton>
+                                                <span>{this.state.liked[this.state.indexImageSelectedForDetails] ? this.state.imageSelectedForDetails.likes.count + 1 : this.state.imageSelectedForDetails.likes.count} likes</span>
+                                            </CardActions>
+                                            <Grid className="comment-add-section" container spacing={3}>
+                                                <Grid item xs={10}>
+                                                    <TextField id="add-user-comment-image"
+                                                               className="add-comment-text-field" label="Add a comment"
+                                                               fullWidth={true}/>
+                                                </Grid>
+                                                <Grid item xs={2} className="add-button-grid">
+                                                    <Button className="add-button"
+                                                            variant="contained"
+                                                            id="add-comments-button"
+                                                            color="primary"
+                                                            onClick={this.onAddComment}>Add</Button>
+                                                </Grid>
+                                            </Grid>
+                                        </div>
+                                    ) : null}
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Modal>
                 </Container>
             </div>
-
         }
     }
-
     fetchOwnerInfo = () => {
         let data = null;
         let xhr = new XMLHttpRequest();
@@ -186,10 +283,8 @@ import EditIcon from '@material-ui/icons/Edit'
         });
         let url = this.baseUrl + "users/self/media/recent/?access_token=" + sessionStorage.getItem("access-token");
         xhr.open("GET", url);
-
         xhr.send(data);
     }
-
     onChangeEditFullName = (e) => {
         if (e.target.value === '') {
             this.setState({newFullName: e.target.value, fullNameRequired: true})
@@ -197,7 +292,6 @@ import EditIcon from '@material-ui/icons/Edit'
             this.setState({newFullName: e.target.value, fullNameRequired: false})
         }
     }
-
     onUpdateFullName = () => {
         if (this.state.newFullName == null || this.state.newFullName.trim() === "") {
             this.setState({
@@ -208,19 +302,37 @@ import EditIcon from '@material-ui/icons/Edit'
                 fullName: this.state.newFullName,
                 fullNameRequired: false,
                 newFullName:''
+                newFullName: ''
             })
 
             this.closeEditFullNameModal();
         }
-
     }
-
     openEditFullNameModal = () => {
         this.setState({openFullNameEditModal: true, closeFullNameEditModal: false})
     }
-
     closeEditFullNameModal = () => {
         this.setState({openFullNameEditModal: false, closeFullNameEditModal: true})
+    }
+
+    onImageClickForDetails = (image, index) => {
+        this.setState({imageSelectedForDetails: image, indexImageSelectedForDetails: index})
+        this.openImageDetailsModal()
+    }
+
+    openImageDetailsModal = () => {
+        this.setState({openImageDetailModal: true, closeImageDetailModal: false})
+    }
+
+    closeImageDetailsModal = () => {
+        this.setState({openImageDetailModal: false, closeImageDetailModal: true})
+    }
+
+    onLikeSelectedImage = () => {
+        let index = this.state.indexImageSelectedForDetails;
+        let l = this.state.liked;
+        l[index] = !l[index];
+        this.setState({liked: l})
     }
 }
 
