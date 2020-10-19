e919d86d9784be5d0a0c7ed0c79a69567875a8d5
manaswinisreekar <you@example.com>
2020-10-19 16:14:03 +0530

    removed bugs in the home.js

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -36,58 +36,46 @@ class Home extends Component {
         console.log(this.state.recent_media)
         if (this.props.location.state === undefined) {
             return <Redirect to='/'/>
-        }
-        if (this.props.location.state.loginSuccess === true) {
-            return <div>
-                <div><Header isLoggedIn={true} profilePictureUrl={this.state.profile_picture}/></div>
-                <div className='posts-card-container'>
-                    {
-                        (this.state.recent_media || []).map((details, index) => (
-                            <Card key={details.id} className='post-card'>
-                            <Card key={details.id+'_card'} className='post-card'>
-                                <CardHeader avatar={<Avatar src={details.user.profile_picture}/>}
-                                            title={details.user.username}
-                                            subheader={new Date(details.created_time * 1000).toLocaleString()}/>
-                                <CardContent>
-                                    <img className='post-image' src={details.images.standard_resolution.url}/>
-                                    <img alt={details.id+'_image'} className='post-image' src={details.images.standard_resolution.url}/>
-                                    <hr/>
-                                    <div className='post-caption'>{details.caption.text.split("\n")[0]}</div>
-                                    {details.tags.map((tag, index) => (
-                                        <span key={index}><a className='post-tags'
-                                                             href={tag}>{'#' + tag + ' '}</a></span>)
-                                    )}
-                                    <br/>
-                                    <div className='likes'>
-                                        <FavoriteBorderIcon fontSize='default'/>
-                                        <pre> </pre>
-                                        <span>{details.likes.count + ' likes'}</span>
-                                    </div>
-                                    <div className='post-comment'>
-                                        <FormControl className='post-comment-form-control'>
-                                            <InputLabel htmlFor='comment'>Add a comment</InputLabel>
-                                            <Input className='comment-input' type='text'></Input>
-                                        </FormControl>
-                                        <div className='add-button'>
-                                            <FormControl>
-                                                <Button variant='contained' color='primary'>ADD</Button>
-                                            </FormControl>
-                                        </div>
-                                    </div>
-                                </CardContent>
-                            </Card>
-                        ))
-                    }
-                </div>
-            </div>
-        }
-    }
-    fetchOwnerInfo = () => {
-        let data = null;
-        let xhr = new XMLHttpRequest();
-        let that = this;
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                that.setState({profile_picture: JSON.parse(this.responseText).data.profile_picture});
+        }render() {
+            console.log(this.state.recent_media)
+            if (this.props.location.state === undefined) {
+                return <Redirect to='/'/>
             }
-
+            if (this.props.location.state.loginSuccess === true) {
+                return <div>
+                    <div><Header isLoggedIn={true} profilePictureUrl={this.state.profile_picture}/></div>
+                    <div className='posts-card-container'>
+                        {
+                            (this.state.recent_media || []).map((details, index) => (
+                                <Card key={details.id+'_card'} className='post-card'>
+                                    <CardHeader avatar={<Avatar src={details.user.profile_picture}/>}
+                                                title={details.user.username}
+                                                subheader={new Date(details.created_time * 1000).toLocaleString()}/>
+                                    <CardContent>
+                                        <img alt={details.id+'_image'} className='post-image' src={details.images.standard_resolution.url}/>
+                                        <hr/>
+                                        <hr className='horizontal-rule'/>
+                                        <div className='post-caption'>{details.caption.text.split("\n")[0]}</div>
+                                        {details.tags.map((tag, index) => (
+                                            <span key={index}><a className='post-tags'
+                                                                 href={tag}>{'#' + tag + ' '}</a></span>)
+                                        )}
+                                        <br/>
+                                        <div className='likes'>
+                                            <FavoriteBorderIcon fontSize='default'/>
+                                            <pre> </pre>
+                                            <span>{details.likes.count + ' likes'}</span>
+                                        </div>
+                                        <div className='post-comment'>
+                                            <FormControl className='post-comment-form-control'>
+                                                <InputLabel htmlFor='comment'>Add a comment</InputLabel>
+                                                <Input className='comment-input' type='text'></Input>
+                                            </FormControl>
+                                            <div className='add-button'>
+                                                <FormControl>
+                                                    <Button variant='contained' color='primary'>ADD</Button>
+                                                </FormControl>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+    
