b73354b2dec9175d222d2425e3312b303ed057b7
manaswinisreekar <you@example.com>
2020-10-19 15:39:39 +0530

    changes done in the  homepage

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,31 +1,49 @@
-//import React, {Component} from 'react';
+class Home extends Component {
 
-import Header from "../../common/header/Heaer";
-import {Redirect} from 'react-router-dom';
-
-/*Home Page*/
-
-class Home extends Component{
-    render(){
-        return<span>HomePage</span>
-        if(this.props.location.state===undefined){
-            return <Redirect to ='/' />
-
-        }
-        if(this.props.location.state.loginSuccess===true){
-            return <div>
-                <div>Header</div>
-                <div><span>Succeess</span></div>
-            </div>
-            }
-            if (this.props.location.state.loginSuccess === true) {
-                return <div>
-                    <div><Header/></div>
-                    <div><Header isLoggedIn={true}/></div>
-                    <div><span>Success</span></div>
-                </div>
-            }
+    constructor() {
+        super();
+        this.baseUrl = 'https://api.instagram.com/v1/';
+        this.state = {
+            profile_picture: ''
         }
     }
+
+    componentDidMount() {
+        this.fetchOwnerInfo();
+    }
+
+    render() {
+        if (this.props.location.state === undefined) {
+            return <Redirect to='/'/>
+        }
+        if (this.props.location.state.loginSuccess === true) {
+            return <div>
+                <div><Header isLoggedIn={true}/></div>
+                <div><Header isLoggedIn={true} profilePictureUrl={this.state.profile_picture}/></div>
+                <div><span>Success</span></div>
+            </div>
+        }
+    }
+
+    fetchOwnerInfo = () => {
+        let data = null;
+
+        let xhr = new XMLHttpRequest();
+
+        let that = this;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({profile_picture: JSON.parse(this.responseText).data.profile_picture});
+            }
+        });
+
+        let url = this.baseUrl+"users/self/?access_token=" + sessionStorage.getItem("access-token");
+
+        xhr.open("GET", url);
+
+        xhr.send(data);
+    }
 }
-export default Home;
+
+export default Home; 
